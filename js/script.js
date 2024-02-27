2057140a4a7ae335a039bd9464a98317407afec7
s4r44d43 <sararez@live.it>
2024-02-27 18:51:22 +0100

    [another try]

--- js/script.js
+++ js/script.js
@@ -20,29 +20,41 @@ const list = [
 // lista di prodotti estratti
 const listDone = [];
 
+let counter = 0;
+
 // nome eatrtto? no quindi è false 
 let productToTake = false;
 
 while( !productToTake ) {
   
   // estraggo un numero (max num = lunghezza elementi lista) che associo agli elementi della lista 
-  const numRan = Math.floor( Math.random () * list.length );
-  let productEx = list[numRan];
+
+  // const numRan = Math.floor( Math.random () * list.length );
+  // let productEx = list[numRan];
+
 
   // pusho il prodotto estratto nell arrey vuota se non è inclusa già
-  if ( !listDone.includes(productEx)) {
-    listDone.push(productEx);
-    console.log(listDone);
-  };
-  
-  //l condizione nella quale si ferma il loop
-  if (listDone.length === list.length){
-    productToTake = true; 
-  }
-};
+  // if ( !listDone.includes(productEx)) {
+  //   listDone.push(productEx);
+  //   console.log(listDone);
+  // };
 
+  let productEx = list [counter];
+
+  listDone.push(productEx);
+  
   //stampo in html dentro all ul .list il prodotto estratto
   const listCourses = document.querySelector('.list' );
   listCourses.innerHTML = `<li>${listDone}</li>`;
+
+
+  /// condizione nella quale si ferma il loop
+  if (listDone.length === list.length){
+    productToTake = true; 
+  }
+
+  productEx = list[counter++];
+};
+
 
 
