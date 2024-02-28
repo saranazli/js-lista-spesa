989359f6b0bb1b3638b9da6cce2082f1cbaafa72
s4r44d43 <sararez@live.it>
2024-02-28 10:09:53 +0100

    [correction] exercise

--- js/script.js
+++ js/script.js
@@ -1,11 +1,4 @@
 
-/*
-Data una lista della spesa
-stampare sulla pagina
-gli elementi della lista individualmente con un ciclo while.
-*/
-
-// la lista della spesa da cui estrarre i prodotti e stampare in html
 const list = [
   'carote',
   'frutta',
@@ -17,44 +10,16 @@ const list = [
   'pesce'
 ];
 
-// lista di prodotti estratti
-const listDone = [];
-
 let counter = 0;
 
-// nome eatrtto? no quindi è false 
-let productToTake = false;
-
-while( !productToTake ) {
-  
-  // estraggo un numero (max num = lunghezza elementi lista) che associo agli elementi della lista 
-
-  // const numRan = Math.floor( Math.random () * list.length );
-  // let productEx = list[numRan];
+const listCourses = document.querySelector('.list' );
+console.log(listCourses);
 
 
-  // pusho il prodotto estratto nell arrey vuota se non è inclusa già
-  // if ( !listDone.includes(productEx)) {
-  //   listDone.push(productEx);
-  //   console.log(listDone);
-  // };
+ while( counter < list.length ) {
 
-  let productEx = list [counter];
+  listCourses.innerHTML += `<li>${list[counter]}</li>`;
 
-  listDone.push(productEx);
-  
-  //stampo in html dentro all ul .list il prodotto estratto
-  const listCourses = document.querySelector('.list' );
-  listCourses.innerHTML = `<li>${listDone}</li>`;
+  counter++;
 
-
-  /// condizione nella quale si ferma il loop
-  if (listDone.length === list.length){
-    productToTake = true; 
-  }
-
-  productEx = list[counter++];
-};
-
-
-
+ };
